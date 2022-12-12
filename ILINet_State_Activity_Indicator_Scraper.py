350fcef1b5b496ac359a7f3496f56fe1355e192a
Anique Khawar <60527604+aniquekhawar@users.noreply.github.com>
2022-12-12 16:51:40 -0500

    initial commit

---
+++ ILINet_State_Activity_Indicator_Scraper.py
@@ -0,0 +1,90 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.options import Options
+import time
+import glob
+import os
+
+# commands to get the current directory (which is usually the directory to the Git Repository)
+parent_directory = os.path.dirname(os.path.realpath(__file__))
+print(parent_directory)
+
+# function from Stackoverflow Article User Austin Mackillop @ https://stackoverflow.com/a/51949811
+def download_wait(directory, timeout, nfiles=None):
+    """
+    Wait for downloads to finish with a specified timeout.
+
+    Args
+    ----
+    directory : str
+        The path to the folder where the files will be downloaded.
+    timeout : int
+        How many seconds to wait until timing out.
+    nfiles : int, defaults to None
+        If provided, also wait for the expected number of files.
+
+    """
+    seconds = 0
+    dl_wait = True
+    while dl_wait and seconds < timeout:
+        time.sleep(1)
+        dl_wait = False
+        files = os.listdir(directory)
+        if nfiles and len(files) != nfiles:
+            dl_wait = True
+
+        for fname in files:
+            if fname.endswith('.crdownload'):
+                dl_wait = True
+
+        seconds += 1
+    return seconds
+
+def remove_csvs(parent_directory):
+    remove_zips = glob.glob(f'{parent_directory}/*.csv')
+    for filePath in remove_zips:
+        try:
+            os.remove(filePath)
+            print(f'File at {filePath} removed successfully.')
+        except:
+            print('Error while deleting file: ', filePath)
+remove_csvs(parent_directory)
+
+opts = Options()
+opts.add_argument("--headless")
+opts.set_preference("browser.download.folderList", 2)
+opts.set_preference("browser.download.manager.showWhenStarting", False)
+opts.set_preference("browser.download.dir", parent_directory)
+opts.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
+
+browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options = opts)
+
+url = 'https://gis.cdc.gov/grasp/fluview/main.html'
+browser.get(url)
+time.sleep(10)
+
+download_btn_xpath = '//button[@ng-click="mainCtrl.showDownloadPopup()"]'
+browser.find_element('xpath', download_btn_xpath).click()
+time.sleep(3)
+
+custom_radio_btn_xpath = '//*[@id="downloadDataBody"]/ul[1]/li[2]/div/ul/li/a'
+browser.find_element('xpath', custom_radio_btn_xpath).click()
+time.sleep(3)
+
+all_seasons_xpath = '//*[@id="customDownloadPanel"]/li[1]/div/div[3]/li/a'
+browser.find_element('xpath', all_seasons_xpath).click()
+time.sleep(3)
+
+download_csv_btn_xpath = '//button[@ng-click="mainCtrl.getCsvFile()"]'
+browser.find_element('xpath', download_csv_btn_xpath).click()
+download_wait(parent_directory, 300)
+
+browser.close()
+
+filename = glob.glob(f'{parent_directory}/*.csv')[0]
+print(filename)
+os.rename(filename, 'ilinet_state_activity_indicator_map_data.csv')
+filename2 = glob.glob(f'{parent_directory}/*.csv')[0]
+print(filename2)
+print('Exiting program.')
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ILINet_State_Activity_Indicator_Scraper
- 
+
+> The purpose of this scraper is to retrieve a dataset for the ILINet State Activity Levels map. The site can be found [here](https://gis.cdc.gov/grasp/fluview/main.html).
